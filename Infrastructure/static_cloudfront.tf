acf24dec6f632748aeae5b282c70c4aa31dfb824
nathanielcx <51494277+nathanielcx@users.noreply.github.com>
2024-02-02 15:14:52 -0800

    Update static_cloudfront.tf

--- Infrastructure/static_cloudfront.tf
+++ Infrastructure/static_cloudfront.tf
@@ -47,10 +47,10 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     viewer_protocol_policy = "redirect-to-https"
     cache_policy_id        = data.aws_cloudfront_cache_policy.Managed-CachingOptimized.id
     compress               = true
-    function_association {
-      event_type   = "viewer-request"
-      function_arn = aws_cloudfront_function.redirect.arn
-    }
+    #function_association {
+    #  event_type   = "viewer-request"
+    #  function_arn = aws_cloudfront_function.redirect.arn
+    #}
     min_ttl     = 0
     default_ttl = 3600
     max_ttl     = 86400
@@ -64,10 +64,10 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     viewer_protocol_policy = "redirect-to-https"
     cache_policy_id        = data.aws_cloudfront_cache_policy.Managed-CachingOptimized.id
     compress               = true
-    function_association {
-      event_type   = "viewer-request"
-      function_arn = aws_cloudfront_function.redirect.arn
-    }
+    #function_association {
+    #  event_type   = "viewer-request"
+    #  function_arn = aws_cloudfront_function.redirect.arn
+    #}
   }
 
   price_class = "PriceClass_100"
