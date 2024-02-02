dbdeeeb58622ef3c89e08dfd8482360f5a4c8834
nathanielcx <51494277+nathanielcx@users.noreply.github.com>
2024-02-02 14:46:50 -0800

    Update s3.tf

--- Infrastructure/s3.tf
+++ Infrastructure/s3.tf
@@ -1,7 +1,3 @@
-resource "aws_s3_bucket" "sql_scripts" {
-  bucket = "${data.aws_caller_identity.current.account_id}-sql-scripts"
-}
-
 resource "aws_s3_bucket" "static" {
   bucket = "phlat-${var.target_env}"
 
