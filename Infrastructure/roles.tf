e6328178e6ed04a3e0e7b5f786198656d057efdd
nathanielcx <51494277+nathanielcx@users.noreply.github.com>
2024-02-02 16:11:18 -0800

    Update roles.tf

--- Infrastructure/roles.tf
+++ Infrastructure/roles.tf
@@ -19,7 +19,7 @@ data "aws_iam_policy_document" "ec2_assume_role_policy" {
 }
 
 resource "aws_iam_policy" "get_secret" {
-  name = "ECS_GetSecret"
+  name = "PHLAT_ECS_GetSecret"
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
