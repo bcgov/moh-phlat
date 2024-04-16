efa2788603f99144c7bea07826acf35285a40729
Ashwani-cgi <ashwani.kalra@cgi.com>
2024-04-16 10:18:56 -0600

    formatting changes

--- backend/app/src/main/java/com/moh/phlat/backend/security/OpenAPIConfig.java
+++ backend/app/src/main/java/com/moh/phlat/backend/security/OpenAPIConfig.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,24 +17,21 @@ public class OpenAPIConfig {
     @Bean
     public OpenAPI customizeOpenAPI() {
         final String securitySchemeName = "bearerAuth";
-        Server server=new Server();
+        Server server = new Server();
         server.setUrl("/");
-
-
         return new OpenAPI().servers(List.of(server))
-                .addSecurityItem(new SecurityRequirement()
-                                         .addList(securitySchemeName))
-                .components(new Components().addSecuritySchemes(securitySchemeName, createAPIKeyScheme()));
+                            .addSecurityItem(new SecurityRequirement()
+                                                     .addList(securitySchemeName))
+                            .components(new Components()
+                                                .addSecuritySchemes(securitySchemeName, createAPIKeyScheme()));
 
 
     }
-
 
     private SecurityScheme createAPIKeyScheme() {
         return new SecurityScheme().type(SecurityScheme.Type.HTTP)
                                    .bearerFormat("JWT")
                                    .scheme("bearer");
     }
-
 
 }
