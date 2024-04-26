2dba94a0dc3e94fe0458d234fe2a5a35fc2d67aa
ptugger <prabhjot.tugger@cgi.com>
2024-04-26 11:06:22 -0600

    Remove commented code

--- frontend/app/src/services/statusService.js
+++ frontend/app/src/services/statusService.js
@@ -1,5 +1,4 @@
 import { appAxios } from '~/services/interceptors';
-// import { ApiRoutes } from '~/utils/constants';
 
 export default {
   async serviceGetAllStatus(includeDeleted = false) {
@@ -8,13 +7,4 @@ export default {
   async serviceGetStatusById(id) {
     return appAxios().get(`status/view/${id}`);
   },
-  // async serviceAddStatus(data) {
-  //   return appAxios().post(`status/add`, data);
-  // },
-  // async serviceUpdateStatus(id, data) {
-  //   return appAxios().put(`/status/update/${id}`, data);
-  // },
-  // async serviceDeleteStatusById(id) {
-  //   return appAxios().delete(`status/delete/${id}`);
-  // },
 };
