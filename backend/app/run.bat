10c45b48805dde10154b51341da0f76e6566cf59
lyson-cgi <ly-son.le@cgi.com>
2024-05-22 14:31:45 -0700

    Changed do-not-load-flag

---
+++ backend/app/run.bat
@@ -0,0 +1,6 @@
+set KEYCLOAK_CLIENT_ID=PLR-QA-REGADMIN
+set KEYCLOAK_CLIENT_SECRET=vHT05IJ7Pd19mHImn3TVHuTqr7Jd1
+set PLR_API_HOST=https://plresb-conf.hlth.gov.bc.ca
+set KEYCLOAK_CLIENT_ID_WEB=PHLAT-WEB
+set KEYCLOAK_PROVIDER_URL=https://common-logon-test.hlth.gov.bc.ca/auth/realms/moh_applications
+mvnw.cmd spring-boot:run
