16fe7d092b2ffe98d9fc45787487e970c8e46bfa
Ashwani-cgi <ashwani.kalra@cgi.com>
2024-06-06 22:51:21 -0400

    New Env variable for plr keycloak

--- backend/app/src/main/java/com/moh/phlat/backend/esb/boundary/PlrEsbBoundary.java
+++ backend/app/src/main/java/com/moh/phlat/backend/esb/boundary/PlrEsbBoundary.java
@@ -1,12 +1,17 @@
 package com.moh.phlat.backend.esb.boundary;
 
-import java.net.http.HttpClient;
-
-import javax.net.ssl.SSLContext;
-
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.moh.phlat.backend.esb.json.MaintainFacilityRequest;
+import com.moh.phlat.backend.esb.json.MaintainFacilityResponse;
+import com.moh.phlat.backend.esb.json.MaintainHdsRequest;
+import com.moh.phlat.backend.esb.json.MaintainHdsResponse;
+import com.moh.phlat.backend.model.Control;
+import com.moh.phlat.backend.model.ProcessData;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ssl.NoSuchSslBundleException;
@@ -15,33 +20,26 @@ import org.springframework.boot.ssl.SslBundles;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.JdkClientHttpConnector;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import com.moh.phlat.backend.model.Control;
-import com.moh.phlat.backend.model.ProcessData;
-
-import jakarta.annotation.PostConstruct;
 import reactor.core.publisher.Mono;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonToken;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.moh.phlat.backend.esb.json.MaintainFacilityRequest;
-import com.moh.phlat.backend.esb.json.MaintainFacilityResponse;
-import com.moh.phlat.backend.esb.json.MaintainHdsRequest;
-import com.moh.phlat.backend.esb.json.MaintainHdsResponse;
+import javax.net.ssl.SSLContext;
+import java.net.http.HttpClient;
 
 @Component
 public class PlrEsbBoundary {
 	
 	private static final Logger logger = LoggerFactory.getLogger(PlrEsbBoundary.class);
 	private WebClient webClient;
+
 	private PlrToken token;
 	
 	private SSLContext sslContext;
+
 	@Autowired
     public void initSslContext(SslBundles sslBundles) throws NoSuchSslBundleException {
-        SslBundle sslBundle = sslBundles.getBundle("client");
+		SslBundle sslBundle = sslBundles.getBundle("client");
         sslContext = sslBundle.createSslContext();
     }
 	
@@ -51,15 +49,15 @@ public class PlrEsbBoundary {
 	private String plrBoundaryOrgId;
 	@Value("${plr.boundary.role}")
 	private String plrBoundaryRole;
-	
-	@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+
+	@Value("${plr.boundary.keycloak.provider-url}")
 	private String keyCloakUrl;
 	@Value("${plr.boundary.keycloak.client-id}")
 	private String plrBoundaryClientId;
 	@Value("${plr.boundary.keycloak.client-secret}")
 	private String plrBoundaryClientSecret;
 	
-	private String plrBoundaryEndpoint = "/HSA-web/rs/passthrough/maintain";
+	private final String plrBoundaryEndpoint = "/HSA-web/rs/passthrough/maintain";
 	
 	@PostConstruct
 	public void initPlrEsbBoundary() {
