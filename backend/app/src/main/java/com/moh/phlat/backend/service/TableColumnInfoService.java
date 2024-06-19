d53d3fe0e5f74470192ecce040961efa007c68f2
lyson-cgi <ly-son.le@cgi.com>
2024-06-19 12:37:44 -0700

    removed unused import

--- backend/app/src/main/java/com/moh/phlat/backend/service/TableColumnInfoService.java
+++ backend/app/src/main/java/com/moh/phlat/backend/service/TableColumnInfoService.java
@@ -1,8 +1,6 @@
 package com.moh.phlat.backend.service;
 
 import com.moh.phlat.backend.service.dto.UiColumnName;
-import com.moh.phlat.backend.model.TableColumnInfo;
-
 import java.util.List;
 
 public interface TableColumnInfoService {
