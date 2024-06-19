17614e6516505579254ef250288f362c5c13a88c
Ashwani-cgi <ashwani.kalra@cgi.com>
2024-06-18 21:11:10 -0400

    removed console logs

--- Infrastructure/cloudfront-functions/response-headers.js
+++ Infrastructure/cloudfront-functions/response-headers.js
@@ -1,12 +1,11 @@
 function handler(event) {
     var response = event.response;
     var headers = response.headers;
-    console.log("Original Headers: ", JSON.stringify(headers));
+
     // values as recommended by https://content-security-policy.com/ section "Starter Policy"
     headers['content-security-policy'] = {
              value: "default-src 'self' https://common-logon-dev.hlth.gov.bc.ca https://common-logon-test.hlth.gov.bc.ca https://common-logon.hlth.gov.bc.ca; img-src 'self'; connect-src 'self'; style-src 'self'; script-src 'self'; base-uri 'self'; form-action 'self'; frame-ancestors 'self'"
     };
 
-    console.log("Modified Headers: ", JSON.stringify(headers));
     return response;
 }
