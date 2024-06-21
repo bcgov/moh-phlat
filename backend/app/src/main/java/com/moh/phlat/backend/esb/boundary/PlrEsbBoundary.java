780a7f82fb15f9a85fa7896ca79cba97b549fbdd
Ashwani-cgi <ashwani.kalra@cgi.com>
2024-06-20 22:59:27 -0400

    removed the keycloak url from logging

--- backend/app/src/main/java/com/moh/phlat/backend/esb/boundary/PlrEsbBoundary.java
+++ backend/app/src/main/java/com/moh/phlat/backend/esb/boundary/PlrEsbBoundary.java
@@ -156,7 +156,6 @@ public class PlrEsbBoundary {
 	}
 	
 	private PlrToken getPlrKeyCloakDetails() {
-		logger.info("PLR Keycloak url is {} ", keyCloakUrl);
 		WebClient webClient = WebClient.builder()
 				.baseUrl(keyCloakUrl)
 				.clientConnector(new JdkClientHttpConnector(HttpClient.newHttpClient()))
