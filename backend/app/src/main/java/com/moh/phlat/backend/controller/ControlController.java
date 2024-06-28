9a140d15423ef380680db05b99de9ca1d625c2d2
bsdrever <98039880+bsdrever@users.noreply.github.com>
2024-06-28 15:57:32 -0700

    Removed all changes concerning the controller.
    Replaced with the code in Main.

--- backend/app/src/main/java/com/moh/phlat/backend/controller/ControlController.java
+++ backend/app/src/main/java/com/moh/phlat/backend/controller/ControlController.java
@@ -1,8 +1,7 @@
+
 package com.moh.phlat.backend.controller;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,18 +10,14 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.moh.phlat.backend.model.Control;
 import com.moh.phlat.backend.repository.ControlRepository;
 import com.moh.phlat.backend.response.ResponseMessage;
-import com.moh.phlat.backend.service.ControlService;
-import com.moh.phlat.backend.service.ProcessDataService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,10 +30,6 @@ public class ControlController {
 
 	@Autowired
 	private ControlRepository controlRepository;
-	
-	@Autowired
-	private ControlService controlService;
-	
 	@PreAuthorize("hasAnyRole(@roleService.getAllRoles())")
 	@GetMapping("/view/all")
 	public @ResponseBody ResponseEntity<ResponseMessage> getAllControls() {
@@ -50,21 +41,21 @@ public class ControlController {
 	@PreAuthorize("hasAnyRole(@roleService.getAllRoles())")
 	@GetMapping("/view/{id}")
 	public ResponseEntity<ResponseMessage> getControlById(@PathVariable Long id) {
-		List<Control> controlTable = controlService.findById(id);
+		Optional<Control> controlTable = controlRepository.findById(id);
 		if (controlTable.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND)
 					.body(new ResponseMessage("success", 404, "Control table not found with id: " + id, "[]"));
 		}
 
 		return ResponseEntity.status(HttpStatus.OK)
-				.body(new ResponseMessage("success", 200, "", controlService.findById(id)));
+				.body(new ResponseMessage("success", 200, "", controlRepository.findById(id)));
 	}
 
 	@PreAuthorize("hasAnyRole(@roleService.getAllRoles())")
 	@GetMapping("/view/filename/{fileName}")
 	public ResponseEntity<ResponseMessage> getControlByFileName(@PathVariable String fileName) {
 		return ResponseEntity.status(HttpStatus.OK)
-				.body(new ResponseMessage("success", 200, "", controlService.findByFileName(fileName)));
+				.body(new ResponseMessage("success", 200, "", controlRepository.findByFileName(fileName)));
 
 	}
 
@@ -72,7 +63,7 @@ public class ControlController {
 	@PutMapping("/update/{id}")
 	public ResponseEntity<ResponseMessage> updateControl(@PathVariable("id") long id,
 			@RequestBody Control requestControl) {
-		List<Control> controlTableData = controlService.findById(id);
+		Optional<Control> controlTableData = controlRepository.findById(id);
 
 		if (controlTableData.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND)
@@ -81,7 +72,7 @@ public class ControlController {
 
 		try {
 
-			Control _controlTable = controlTableData.get(0);
+			Control _controlTable = controlTableData.get();
 
 			_controlTable.setFileName(requestControl.getFileName());
 			_controlTable.setUserId(AuthenticationUtils.getAuthenticatedUserId());
@@ -141,4 +132,6 @@ public class ControlController {
 					"Internal error encountered while approving constrol table with id: " + id, "[]"));
 		}
 	}
+
+	
 }
--- backend/app/src/main/java/com/moh/phlat/backend/repository/ControlRepository.java
+++ backend/app/src/main/java/com/moh/phlat/backend/repository/ControlRepository.java
@@ -3,17 +3,10 @@ package com.moh.phlat.backend.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.lang.NonNull;
-import org.springframework.stereotype.Repository;
 
 import com.moh.phlat.backend.model.Control;
 
-@Repository
-public interface ControlRepository extends JpaRepository<Control, Long> {
 
-	public List<Control> findAllById(Long id);
-	
+public interface ControlRepository extends JpaRepository<Control, Long> {
 	public List<Control> findByFileName(String fileName);
-	
-	public @NonNull List<Control> findAll();
 }
